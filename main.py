fe6eede7bee173fb2f69effae9f556956ea812e9
vignesh2736@gmail.com <vignesh2736@gmail.com>
2021-05-30 20:53:56 +0530

    Commit Requirement changes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ Created on Sun May 30 14:04:01 2021
 """
 from flask import Flask
   
-app = Flask(__name__)
+main = Flask(__name__)
   
-@app.route("/")
+@main.route("/")
 def home_view():
         return "<h1>Welcome to Geeks for Geeks</h1>"
