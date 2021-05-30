7cddfb4d6d3e69e2c10e547528edfb78616be41f
unknown <vignesh2736@gmail.com>
2021-05-30 15:03:59 +0530

    My changes

---
+++ app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 14:04:01 2021
+
+@author: Vignez
+"""
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'hello world'
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0')
